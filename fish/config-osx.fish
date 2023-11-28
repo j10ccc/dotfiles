405ebdcf1a4b857b6db697d130954e6038dd2a3d
j10c <blyb1739@gmail.com>
2023-11-28 10:43:05 +0800

    refactor(fish): remove outdated config
    Split configuration by os `uname`

---
+++ fish/config-osx.fish
@@ -0,0 +1,11 @@
+alias pro "cd /Volumes/Code/projects"
+alias play "cd /Volumes/Code/playground"
+alias homework "cd /Volumes/Code/homeworks"
+
+# Pnpm
+export PNPM_HOME="/Users/j10c/Library/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+
+# Maven
+export M2_HOME="/etc/maven/apache-maven-3.9.0"
+export PATH="$M2_HOME/bin:$PATH"
--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,21 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+set fish_greeting ""
 
-alias pro "cd /Volumes/Code/projects"
-alias brew "/opt/homebrew/bin/brew"
 alias ls "exa"
 alias ll "exa -l"
 
-export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
-
-export NVM_DIR="$HOME/.nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
-nvm use 16
-
-export PNPM_HOME="/Users/j10c/Library/pnpm"
-export PATH="$PNPM_HOME:$PATH"
-
-export M2_HOME="/etc/maven/apache-maven-3.9.0"
-export PATH="$M2_HOME/bin:$PATH"
-
+# Jenv
 eval "$(jenv init -)"
+
+# Cross-platform config
+switch (uname)
+  case Darwin
+    source (dirname (status --current-filename))/config-osx.fish
+  case Linux
+    source (dirname (status --current-filename))/config-linux.fish
+end
+
+# Local config
+set FISH_LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $FISH_LOCAL_CONFIG
+  source $FISH_LOCAL_CONFIG
+end

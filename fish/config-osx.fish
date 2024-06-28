8640bff8c3887ee64ae0a468ab5e2170269f7a10
j10c <blyb1739@gmail.com>
2024-06-28 23:52:15 +0800

    feat(fish): use dynamic directory

--- fish/config-osx.fish
+++ fish/config-osx.fish
@@ -3,9 +3,17 @@ alias play "cd /Volumes/Code/playground"
 alias homework "cd /Volumes/Code/homeworks"
 
 # Pnpm
-export PNPM_HOME="/Users/j10c/Library/pnpm"
+export PNPM_HOME="$HOME/Library/pnpm"
 export PATH="$PNPM_HOME:$PATH"
 
 # Maven
 export M2_HOME="/etc/maven/apache-maven-3.9.0"
 export PATH="$M2_HOME/bin:$PATH"
+
+# Bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+# Android
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export PATH="$ANDROID_HOME/platform-tools:$PATH"

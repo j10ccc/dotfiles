f793ba55ebfeb464364044817424efd38bf4538a
j10c <blyb1739@gmail.com>
2024-11-25 08:57:54 +0800

    feat(nix/devenv): new env for bytedance life dev

---
+++ nix/devenv/bd-life/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Nix configuration for bd-life";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    legacyNixPkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  };
+
+  outputs = { self , nixpkgs , legacyNixPkgs ,... }: let
+    system = "aarch64-darwin";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      legacyPkgs = import legacyNixPkgs {
+        inherit system;
+        config = {
+          permittedInsecurePackages = [ "nodejs-16.20.2" ];
+        };
+      };
+    in pkgs.mkShell {
+      packages = [
+        pkgs.git
+        legacyPkgs.nodejs_16
+        pkgs.pnpm_8
+        pkgs.nushell
+      ];
+
+      shellHook = ''
+        echo "node `${pkgs.nodejs}/bin/node --version`"
+        exec nu
+      '';
+    };
+  };
+}

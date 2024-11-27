fc39c5e933ac25ff6a7dd4ce7b5a007c0883e399
j10c <blyb1739@gmail.com>
2024-11-27 23:46:56 +0800

    feat(nix/devenv): new env for making a living dev

---
+++ nix/devenv/mk-living/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Nix configuration for making a living~";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    system = "aarch64-darwin";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        git
+        nushell
+        nodejs_20
+        pnpm
+        bun
+        nodePackages."@antfu/ni"
+      ];
+
+      shellHook = ''
+        echo "node `${pkgs.nodejs}/bin/node --version`"
+        exec nu
+      '';
+    };
+  };
+}

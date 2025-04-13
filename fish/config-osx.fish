14f2ed27cf37da4d3abd615c833e2eee249152fe
j10c <blyb1739@gmail.com>
2025-04-13 22:17:19 +0800

    feat(fish): remove unused exports and add brew completions
    Remove Maven and Android SDK exports as they are no longer needed. Add brew completions to enhance shell functionality.

--- fish/config-osx.fish
+++ fish/config-osx.fish
@@ -2,14 +2,14 @@
 export PNPM_HOME="$HOME/Library/pnpm"
 export PATH="$PNPM_HOME:$PATH"
 
-# Maven
-export M2_HOME="/etc/maven/apache-maven-3.9.0"
-export PATH="$M2_HOME/bin:$PATH"
-
 # Bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
 
-# Android
-export ANDROID_HOME="$HOME/Library/Android/sdk"
-export PATH="$ANDROID_HOME/platform-tools:$PATH"
+if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end

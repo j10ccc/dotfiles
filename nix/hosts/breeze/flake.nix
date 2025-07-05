0aff64dcb0f81446c001eb7e4173e43933dcfb74
j10c <blyb1739@gmail.com>
2025-07-05 22:28:26 +0800

    style(nix): format nix files

--- nix/hosts/breeze/flake.nix
+++ nix/hosts/breeze/flake.nix
@@ -8,130 +8,126 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    name = "Breeze";
-    configuration = { pkgs, ... }: {
-      nixpkgs.config.allowUnfree = true;
+    let
+      name = "Breeze";
+      configuration = { pkgs, ... }: {
+        nixpkgs.config.allowUnfree = true;
 
-      environment.systemPackages = with pkgs; [
-        git
-        neovim
-        fish
-        eza
-        zoxide
-        ripgrep
-        fd
-        devbox
-        lazygit
-        vivid
-        nodejs_22
-        pnpm
-        bun
-        nodePackages."@antfu/ni"
-        wezterm
-        localsend
-        whistle
-        zoxide
-        direnv
-        nixfmt-classic
-      ];
-
-      homebrew = {
-        enable = true;
-        brews = [
-          "mas"
+        environment.systemPackages = with pkgs; [
+          git
+          neovim
+          fish
+          eza
+          zoxide
+          ripgrep
+          fd
+          devbox
+          lazygit
+          vivid
+          nodejs_22
+          pnpm
+          bun
+          nodePackages."@antfu/ni"
+          wezterm
+          localsend
+          whistle
+          zoxide
+          direnv
+          nixfmt-classic
         ];
-        casks = [
-          "snipaste"
-          "maccy"
-          "iina"
-          "eloston-chromium"
-          "stats"
-          "the-unarchiver"
-          "telegram"
-          "tencent-lemon"
-          "wetype"
-          "aldente"
-          "trae"
-          "docker"
-          "obsidian"
-          "vscodium"
-          "clash-verge-rev"
+
+        homebrew = {
+          enable = true;
+          brews = [ "mas" ];
+          casks = [
+            "snipaste"
+            "maccy"
+            "iina"
+            "eloston-chromium"
+            "stats"
+            "the-unarchiver"
+            "telegram"
+            "tencent-lemon"
+            "wetype"
+            "aldente"
+            "trae"
+            "docker"
+            "obsidian"
+            "vscodium"
+            "clash-verge-rev"
+          ];
+          masApps = {
+            "TickTick" = 966085870;
+            "WeChat" = 836500024;
+            "QQ" = 451108668;
+            "DingTalk" = 1435447041;
+            "Feishu" = 1551632588;
+          };
+          onActivation.cleanup = "zap";
+        };
+
+        fonts.packages = with pkgs; [
+          noto-fonts-cjk-sans
+          noto-fonts-emoji
+          nerd-fonts.blex-mono
         ];
-        masApps = {
-          "TickTick" = 966085870;
-          "WeChat" = 836500024;
-          "QQ" = 451108668;
-          "DingTalk" = 1435447041;
-          "Feishu" = 1551632588;
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Enable alternative shell support in nix-darwin.
+        programs.fish.enable = true;
+
+        system = {
+          # Set Git commit hash for darwin-version.
+          configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          stateVersion = 5;
+
+          primaryUser = "j10c";
+
+          defaults = {
+            NSGlobalDomain = {
+              ApplePressAndHoldEnabled = false;
+              AppleShowAllExtensions = true;
+              "com.apple.keyboard.fnState" = true;
+            };
+            finder = {
+              ShowPathbar = true;
+              AppleShowAllExtensions = true;
+            };
+            hitoolbox.AppleFnUsageType = "Show Emoji & Symbols";
+            menuExtraClock = {
+              Show24Hour = false;
+              ShowAMPM = true;
+              ShowDate = 2;
+              ShowDayOfWeek = false;
+              ShowSeconds = false;
+              ShowDayOfMonth = false;
+            };
+            dock.wvous-tl-corner = 4; # Show desktop
+          };
+
+          keyboard = {
+            enableKeyMapping = true;
+            remapCapsLockToControl = true;
+          };
         };
-        onActivation.cleanup = "zap";
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+
+        nix.enable = false;
       };
+    in {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations.${name} =
+        nix-darwin.lib.darwinSystem { modules = [ configuration ]; };
 
-      fonts.packages = with pkgs; [
-        noto-fonts-cjk-sans
-        noto-fonts-emoji
-        nerd-fonts.blex-mono
-      ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      programs.fish.enable = true;
-
-      system = {
-        # Set Git commit hash for darwin-version.
-        configurationRevision = self.rev or self.dirtyRev or null; 
-        
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        stateVersion = 5;
-
-        primaryUser = "j10c";
-
-        defaults = {
-          NSGlobalDomain = {
-            ApplePressAndHoldEnabled = false;
-            AppleShowAllExtensions = true;
-            "com.apple.keyboard.fnState" = true;
-          };
-          finder = {
-            ShowPathbar = true;
-            AppleShowAllExtensions = true;
-          };
-          hitoolbox.AppleFnUsageType = "Show Emoji & Symbols";
-          menuExtraClock = {
-            Show24Hour = false;
-            ShowAMPM = true;
-            ShowDate = 2;
-            ShowDayOfWeek = false;
-            ShowSeconds = false;
-            ShowDayOfMonth = false;
-          };
-          dock.wvous-tl-corner = 4; # Show desktop
-        };
-
-        keyboard = {
-          enableKeyMapping = true;
-          remapCapsLockToControl = true;
-        };
-      };
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      nix.enable = false;
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations.${name}.pkgs;
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations.${name} = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations.${name}.pkgs;
-  };
 }
--- nix/hosts/midnight/flake.nix
+++ nix/hosts/midnight/flake.nix
@@ -8,121 +8,116 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    name = "Midnight";
-    configuration = { pkgs, ... }: {
-      nixpkgs.config.allowUnfree = true;
+    let
+      name = "Midnight";
+      configuration = { pkgs, ... }: {
+        nixpkgs.config.allowUnfree = true;
 
-      environment.variables.EDITOR = "nvim";
-      environment.systemPackages = with pkgs; [
-        neovim
-        fish
-        eza
-        zoxide
-        ripgrep
-        nixpkgs-fmt
-        fd
-        devbox
-        lazygit
-        vivid
-        direnv
-        wezterm
-        obsidian
-        vscodium
-        localsend
-        git
-        whistle
-        pnpm
-        nodePackages."@antfu/ni"
-        nodejs_20
-      ];
-
-      homebrew = {
-        enable = true;
-        brews = [
-          "mas"
-          "ast-grep"
+        environment.variables.EDITOR = "nvim";
+        environment.systemPackages = with pkgs; [
+          neovim
+          fish
+          eza
+          zoxide
+          ripgrep
+          nixpkgs-fmt
+          fd
+          devbox
+          lazygit
+          vivid
+          direnv
+          wezterm
+          obsidian
+          vscodium
+          localsend
+          git
+          whistle
+          pnpm
+          nodePackages."@antfu/ni"
+          nodejs_20
         ];
-        casks = [
-          "snipaste"
-          "maccy"
-          "iina"
-          "eloston-chromium"
-          "stats"
-          "the-unarchiver"
-          "telegram"
-          "tencent-lemon"
-          "wetype"
-          "aldente"
-          "mitmproxy"
-          "logi-options+"
-          "apifox"
-          "wpsoffice-cn"
+
+        homebrew = {
+          enable = true;
+          brews = [ "mas" "ast-grep" ];
+          casks = [
+            "snipaste"
+            "maccy"
+            "iina"
+            "eloston-chromium"
+            "stats"
+            "the-unarchiver"
+            "telegram"
+            "tencent-lemon"
+            "wetype"
+            "aldente"
+            "mitmproxy"
+            "logi-options+"
+            "apifox"
+            "wpsoffice-cn"
+          ];
+          masApps = {
+            "TickTick" = 966085870;
+            "Refined Github" = 1519867270;
+            "Feishu" = 1551632588;
+          };
+          onActivation.cleanup = "zap";
+        };
+
+        fonts.packages = with pkgs; [
+          noto-fonts-cjk-sans
+          noto-fonts-cjk-serif
+          noto-fonts-emoji
+          nerd-fonts.blex-mono
         ];
-        masApps = {
-          "TickTick" = 966085870;
-          "Refined Github" = 1519867270;
-          "Feishu" = 1551632588;
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Enable alternative shell support in nix-darwin.
+        programs.fish.enable = true;
+
+        system = {
+          stateVersion = 5;
+          # Set Git commit hash for darwin-version.
+          configurationRevision = self.rev or self.dirtyRev or null;
+
+          defaults = {
+            NSGlobalDomain = {
+              ApplePressAndHoldEnabled = false;
+              AppleShowAllExtensions = true;
+              "com.apple.keyboard.fnState" = true;
+            };
+            finder = {
+              ShowPathbar = true;
+              AppleShowAllExtensions = true;
+            };
+            hitoolbox.AppleFnUsageType = "Show Emoji & Symbols";
+            menuExtraClock = {
+              Show24Hour = false;
+              ShowAMPM = true;
+              ShowDate = 2;
+              ShowDayOfWeek = false;
+              ShowSeconds = false;
+              ShowDayOfMonth = false;
+            };
+            dock.wvous-tl-corner = 4; # Show desktop
+          };
+          keyboard = {
+            enableKeyMapping = true;
+            remapCapsLockToControl = true;
+          };
         };
-        onActivation.cleanup = "zap";
+
+        nixpkgs.hostPlatform = "aarch64-darwin";
       };
+    in {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations.${name} =
+        nix-darwin.lib.darwinSystem { modules = [ configuration ]; };
 
-      fonts.packages = with pkgs; [
-        noto-fonts-cjk-sans
-        noto-fonts-cjk-serif
-        noto-fonts-emoji
-        nerd-fonts.blex-mono
-      ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      programs.fish.enable = true;
-
-      system = {
-        stateVersion = 5;
-        # Set Git commit hash for darwin-version.
-        configurationRevision = self.rev or self.dirtyRev or null;
-
-        defaults = {
-          NSGlobalDomain = {
-            ApplePressAndHoldEnabled = false;
-            AppleShowAllExtensions = true;
-            "com.apple.keyboard.fnState" = true;
-          };
-          finder = {
-            ShowPathbar = true;
-            AppleShowAllExtensions = true;
-          };
-          hitoolbox.AppleFnUsageType = "Show Emoji & Symbols";
-          menuExtraClock = {
-            Show24Hour = false;
-            ShowAMPM = true;
-            ShowDate = 2;
-            ShowDayOfWeek = false;
-            ShowSeconds = false;
-            ShowDayOfMonth = false;
-          };
-          dock.wvous-tl-corner = 4; # Show desktop
-        };
-        keyboard = {
-          enableKeyMapping = true;
-          remapCapsLockToControl = true;
-        };
-      };
-
-      nixpkgs.hostPlatform = "aarch64-darwin";
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations.${name}.pkgs;
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations.${name} = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations.${name}.pkgs;
-  };
 }

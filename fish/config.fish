b1a00d8b562ea79cc94a9db5248dfa91cb47e7d1
j10c <blyb1739@gmail.com>
2025-08-20 15:26:26 +0800

    chore(fish): format indent with editorconfig

--- fish/config.fish
+++ fish/config.fish
@@ -8,16 +8,16 @@ command -v zoxide &> /dev/null && zoxide init fish | source
 
 # Cross-platform config
 switch (uname)
-  case Darwin
-    source (dirname (status --current-filename))/config-osx.fish
-  case Linux
-    source (dirname (status --current-filename))/config-linux.fish
+    case Darwin
+        source (dirname (status --current-filename))/config-osx.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
 end
 
 # Local config
 set FISH_LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $FISH_LOCAL_CONFIG
-  source $FISH_LOCAL_CONFIG
+    source $FISH_LOCAL_CONFIG
 end
 
 export LS_COLORS="$(vivid generate nord)"

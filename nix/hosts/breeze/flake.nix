278adf01c66dc25f564de907b0f433e99a2f6da3
j10c <blyb1739@gmail.com>
2025-09-07 16:09:54 +0800

    refactor(nix): use nix-homebrew to replace self install

--- nix/hosts/breeze/flake.nix
+++ nix/hosts/breeze/flake.nix
@@ -5,9 +5,20 @@
     nixpkgs.url = "github:nixOS/nixpkgs/nixos-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core
+    , homebrew-cask }:
     let
       name = "Breeze";
       configuration = { pkgs, ... }: {
@@ -131,8 +142,30 @@
     in {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#simple
-      darwinConfigurations.${name} =
-        nix-darwin.lib.darwinSystem { modules = [ configuration ]; };
+      darwinConfigurations.${name} = nix-darwin.lib.darwinSystem {
+        modules = [
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+              enableRosetta = false;
+              user = "j10c";
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+              };
+              # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+              mutableTaps = false;
+            };
+          }
+          # Optional: Align homebrew taps config with nix-homebrew
+          ({ config, ... }: {
+            homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
+          })
+          configuration
+        ];
+      };
 
       # Expose the package set, including overlays, for convenience.
       darwinPackages = self.darwinConfigurations.${name}.pkgs;

032a0f8d22d8f8371a97a14acb81196e85856469
j10c <blyb1739@gmail.com>
2025-09-26 10:34:28 +0800

    fix(wezterm): use cjk font for noto sans

--- wezterm/font.lua
+++ wezterm/font.lua
@@ -4,7 +4,7 @@ local module = {};
 function module.apply_to_config(config)
   config.font = wezterm.font_with_fallback {
     "BlexMono Nerd Font Mono",
-    "Noto Sans SC"
+    "Noto Sans CJK SC"
   };
   config.font_size = 16;
 end;

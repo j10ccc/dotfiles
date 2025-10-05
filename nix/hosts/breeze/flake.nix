d3bbb53fc33f1324d3d46a9bcbb382386ebc3be3
j10c <blyb1739@gmail.com>
2025-10-05 11:44:51 +0800

    refactor(nix/breeze): move config to independent file

--- nix/hosts/breeze/flake.nix
+++ nix/hosts/breeze/flake.nix
@@ -19,136 +19,13 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core
     , homebrew-cask }:
-    let
-      name = "Breeze";
-      configuration = { pkgs, ... }: {
-        nixpkgs.config.allowUnfree = true;
-
-        environment.systemPackages = with pkgs; [
-          git
-          neovim
-          fish
-          eza
-          zoxide
-          ripgrep
-          fd
-          devbox
-          lazygit
-          vivid
-          nodejs_22
-          pnpm
-          bun
-          nodePackages."@antfu/ni"
-          wezterm
-          localsend
-          whistle
-          direnv
-          nixfmt-classic
-        ];
-
-        homebrew = {
-          enable = true;
-          brews = [ "mas" ];
-          casks = [
-            "snipaste"
-            "maccy"
-            "iina"
-            "ungoogled-chromium"
-            "stats"
-            "the-unarchiver"
-            "telegram"
-            "tencent-lemon"
-            "aldente"
-            "docker-desktop"
-            "obsidian"
-            "vscodium"
-            "clash-verge-rev"
-            "apifox"
-            "wechatwebdevtools"
-            "adobe-creative-cloud"
-          ];
-          masApps = {
-            "TickTick" = 966085870;
-            "WeChat" = 836500024;
-            "QQ" = 451108668;
-            "Feishu" = 1551632588;
-            "OneDrive" = 823766827;
-          };
-          onActivation = {
-            upgrade = true;
-            cleanup = "zap";
-          };
-        };
-
-        fonts.packages = with pkgs; [
-          noto-fonts-cjk-sans
-          noto-fonts-emoji
-          nerd-fonts.blex-mono
-          nerd-fonts.monaspace
-        ];
-
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-
-        # Enable alternative shell support in nix-darwin.
-        programs.fish.enable = true;
-
-        system = {
-          # Set Git commit hash for darwin-version.
-          configurationRevision = self.rev or self.dirtyRev or null;
-
-          # Used for backwards compatibility, please read the changelog before changing.
-          # $ darwin-rebuild changelog
-          stateVersion = 5;
-
-          primaryUser = "j10c";
-
-          defaults = {
-            NSGlobalDomain = {
-              ApplePressAndHoldEnabled = false;
-              AppleShowAllExtensions = true;
-              "com.apple.keyboard.fnState" = true;
-            };
-            WindowManager.GloballyEnabled = true;
-            finder = {
-              ShowPathbar = true;
-              AppleShowAllExtensions = true;
-            };
-            hitoolbox.AppleFnUsageType = "Show Emoji & Symbols";
-            menuExtraClock = {
-              Show24Hour = false;
-              ShowAMPM = true;
-              ShowDate = 2;
-              ShowDayOfWeek = false;
-              ShowSeconds = false;
-              ShowDayOfMonth = false;
-            };
-            dock = {
-              wvous-tl-corner = 4; # Show desktop
-              autohide = false;
-            };
-            trackpad = {
-              TrackpadThreeFingerDrag = true;
-              TrackpadThreeFingerTapGesture = 2;
-            };
-          };
-
-          keyboard = {
-            enableKeyMapping = true;
-            remapCapsLockToControl = true;
-          };
-        };
-
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "aarch64-darwin";
-
-        nix.enable = false;
-      };
+    let name = "Breeze";
     in {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#simple
       darwinConfigurations.${name} = nix-darwin.lib.darwinSystem {
         modules = [
+          ./configuration.nix
           nix-homebrew.darwinModules.nix-homebrew
           {
             nix-homebrew = {
@@ -168,7 +45,6 @@
           ({ config, ... }: {
             homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
           })
-          configuration
         ];
       };
 

5cb3c622d4540e2127ec5d6713b7b0f765446846
j10c <blyb1739@gmail.com>
2025-10-14 01:28:07 +0800

    feat(fish): bind vi-like key in insert mode

--- modules/fish/functions/fish_hybrid_key_bindings.fish
+++ modules/fish/functions/fish_hybrid_key_bindings.fish
@@ -4,4 +4,9 @@ function fish_hybrid_key_bindings --description \
         fish_default_key_bindings -M $mode
     end
     fish_vi_key_bindings --no-erase
+
+    bind -s -M insert ctrl-h backward-char
+    bind -s -M insert ctrl-l forward-char
+    bind -s -M insert ctrl-j down-line
+    bind -s -M insert ctrl-k up-line
 end
